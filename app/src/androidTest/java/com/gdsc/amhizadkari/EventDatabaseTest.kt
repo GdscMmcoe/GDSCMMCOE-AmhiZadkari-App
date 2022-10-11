8733fa261758975b1ef4af30a095a333143017c8
asyncdoggo <ayushdeshpande81@gmail.com>
2022-10-11 07:47:23 +0530

    Write test for room db

---
+++ app/src/androidTest/java/com/gdsc/amhizadkari/EventDatabaseTest.kt
@@ -0,0 +1,44 @@
+package com.gdsc.amhizadkari
+
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.gdsc.amhizadkari.data.EventDatabase
+import com.gdsc.amhizadkari.data.EventDatabaseDao
+import com.gdsc.amhizadkari.data.EventItem
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class EventDatabaseTest{
+    private lateinit var eventDatabaseDao: EventDatabaseDao
+    private lateinit var db: EventDatabase
+
+    @Before
+    fun createDb(){
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        db = Room.inMemoryDatabaseBuilder(context,EventDatabase::class.java).allowMainThreadQueries().build()
+        eventDatabaseDao = db.eventDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun deleteDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetTest() = runBlocking {
+        val eventItem = EventItem(1,"Independence Day Special","10/10/2022",LoremIpsum(20).values.joinToString())
+        eventDatabaseDao.insert(eventItem)
+        val item = eventDatabaseDao.getEventById(1)
+        assertEquals(item?.eventId,1)
+    }
+}
