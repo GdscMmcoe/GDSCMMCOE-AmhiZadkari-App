4b7472b1b3d20f53323b78c02b57800bf0cd8cf4
asyncdoggo <ayushdeshpande81@gmail.com>
2022-10-14 12:42:21 +0530

    Minor changes

--- app/src/main/java/com/gdsc/amhizadkari/ui/theme/Color.kt
+++ app/src/main/java/com/gdsc/amhizadkari/ui/theme/Color.kt
@@ -10,16 +10,16 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-
-val Colors.linkBlue:Color
-@Composable
-get() = if(isLight) Color(0xff0000ee) else Color(0xff00FFFF)
-
 val Colors.rowColor:Color
 @Composable
 get() = if(isLight) Color(0xffACCAA7) else Color(0xFF0869E0)
 
+
+val Colors.buttonColor:Color
+    @Composable
+    get() = if(isLight) Color(0xFF4CAF50) else Color(0xFF0869E0)
+
 val Colors.CardColor:Color
 @Composable
-get() = if(isLight) Color(0xffD8F2D4) else MaterialTheme.colors.secondary //Color(0xFF047500)
+get() = if(isLight) Color(0xffD8F2D4) else MaterialTheme.colors.secondary
 
