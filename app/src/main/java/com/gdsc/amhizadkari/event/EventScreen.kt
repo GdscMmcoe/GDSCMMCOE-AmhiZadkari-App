3e9e4eb12dfdbd0f1677c877ab7b31cdbb9676f0
asyncdoggo <ayushdeshpande81@gmail.com>
2022-12-03 10:15:50 +0530

    Update EventScreen.kt

--- app/src/main/java/com/gdsc/amhizadkari/event/EventScreen.kt
+++ app/src/main/java/com/gdsc/amhizadkari/event/EventScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,7 +36,7 @@ fun EventScreen(
 ) {
     viewModel.getDetails(LocalContext.current,id)
 
-    val data = remember{ viewModel.details }
+    val data = viewModel.details
 
 
     Scaffold(
